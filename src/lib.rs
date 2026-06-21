875d805117272f9812eb5a9afa0986d9431dffe8
Soroush Mirzaei <soroush.mirzaei@gmail.com>
2026-06-21 13:45:35 -0400

    cargo: make sure docs.rs builds postgres feature

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
+#![cfg_attr(docsrs, feature(doc_cfg))]
+
 //! Reusable components.
 
 pub mod component;
 
+#[cfg_attr(docsrs, doc(cfg(feature = "postgres")))]
 #[cfg(feature = "postgres")]
 pub mod postgres;
